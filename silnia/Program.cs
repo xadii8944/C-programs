06361931014ecada5735c88e7ddaaaacc577eeda
xadii8944 <129669376+xadii8944@users.noreply.github.com>
2024-04-21 18:04:59 +0200

    Add files via upload

---
+++ silnia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Program
+{
+    public static long factorial(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * factorial(n - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int liczba = 4; //Podaj liczbe
+        long wynik = factorial(liczba);
+        Console.WriteLine("Silnia z {0} wynosi {1}", liczba, wynik);
+    }
+}
